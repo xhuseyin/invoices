94246ef672434de646bafbd6dbf10eacc9a07706
xhuseyin <huseyin.cakmak@osf.digital>
2023-04-15 23:10:30 +0300

    Add chakra-ui provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+const fonts = {
+  body: 'Roboto, sans-serif',
+  heading: 'Roboto, sans-serif',
+}
+
+const colors = {
+  primary: {
+    900: '#999',
+    800: '#666',
+    700: '#333',
+  },
+}
+
+const theme = extendTheme({ fonts, colors })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
