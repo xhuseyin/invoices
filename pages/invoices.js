67cf52a2ee5a29663179b84badb8eda251b6d714
xhuseyin <huseyin.cakmak@osf.digital>
2023-04-15 23:30:27 +0300

    Update invoices page with Chakra UI components

--- pages/invoices.js
+++ pages/invoices.js
@@ -1,25 +1,17 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
-const StyledTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-
-  th,
-  td {
-    padding: 0.5rem;
-    text-align: left;
-    border: 1px solid #ddd;
-  }
-
-  th {
-    background-color: #f5f5f5;
-  }
-
-  tbody tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
-`;
+import {
+  Box,
+  Button,
+  Input,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  Container
+} from "@chakra-ui/react";
 
 const InvoicesPage = () => {
   const [invoices, setInvoices] = useState([]);
@@ -103,85 +95,98 @@ const InvoicesPage = () => {
   }, []);
 
   return (
-    <>
-      <h1>Create New Invoice</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="customerName">Customer Name:</label>
-          <input
-            type="text"
-            id="customerName"
-            value={customerName}
-            onChange={(event) => setCustomerName(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="invoiceNumber">Invoice Number:</label>
-          <input
-            type="text"
-            id="invoiceNumber"
-            value={invoiceNumber}
-            onChange={(event) => setInvoiceNumber(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="itemName">Item Name:</label>
-          <input
-            type="text"
-            id="itemName"
-            value={itemName}
-            onChange={(event) => setItemName(event.target.value)}
-          />
-          </div>
-          <div>
-  <label htmlFor="itemPrice">Item Price:</label>
-  <input
-    type="number"
-    id="itemPrice"
-    value={itemPrice}
-    onChange={(event) => setItemPrice(event.target.value)}
-  />
-</div>
-<div>
-  <label htmlFor="itemQuantity">Item Quantity:</label>
-  <input
-    type="number"
-    id="itemQuantity"
-    value={itemQuantity}
-    onChange={(event) => setItemQuantity(event.target.value)}
-  />
-</div>
-<div>
-  <button type="submit">Create Invoice</button>
-  {errorMessage && <p>{errorMessage}</p>}
-</div>
-<StyledTable>
-  <thead>
-    <tr>
-      <th>Customer Name</th>
-      <th>Invoice Number</th>
-      <th>Item Name</th>
-      <th>Item Price</th>
-      <th>Item Quantity</th>
-      <th>Total</th>
-    </tr>
-  </thead>
-  <tbody>
-  {invoices.map((invoice) => (
-  <tr key={invoice.id}>
-    <td>{invoice.customerName}</td>
-    <td>{invoice.invoiceNumber}</td>
-    <td>{invoice.items?.[0]?.name}</td>
-    <td>{invoice.items?.[0]?.price}</td>
-    <td>{invoice.items?.[0]?.quantity}</td>
-    <td>{invoice.total}</td>
-  </tr>
-))}
-
-  </tbody>
-</StyledTable>
-    </form>
-    </>
+<>
+<Container>
+  <Text as="h3" fontSize="3xl" fontWeight="bold" mb="5">
+    Create New Invoice
+  </Text>
+  <Box as="form" onSubmit={handleSubmit}>
+    <Box mb="4">
+      <Text htmlFor="customerName" fontWeight="bold">
+        Customer Name:
+      </Text>
+      <Input
+        type="text"
+        id="customerName"
+        value={customerName}
+        onChange={(event) => setCustomerName(event.target.value)}
+      />
+    </Box>
+    <Box mb="4">
+      <Text htmlFor="invoiceNumber" fontWeight="bold">
+        Invoice Number:
+      </Text>
+      <Input
+        type="text"
+        id="invoiceNumber"
+        value={invoiceNumber}
+        onChange={(event) => setInvoiceNumber(event.target.value)}
+      />
+    </Box>
+    <Box mb="4">
+      <Text htmlFor="itemName" fontWeight="bold">
+        Item Name:
+      </Text>
+      <Input
+        type="text"
+        id="itemName"
+        value={itemName}
+        onChange={(event) => setItemName(event.target.value)}
+      />
+    </Box>
+    <Box mb="4">
+      <Text htmlFor="itemPrice" fontWeight="bold">
+        Item Price:
+      </Text>
+      <Input
+        type="number"
+        id="itemPrice"
+        value={itemPrice}
+        onChange={(event) => setItemPrice(event.target.value)}
+      />
+    </Box>
+    <Box mb="4">
+      <Text htmlFor="itemQuantity" fontWeight="bold">
+        Item Quantity:
+      </Text>
+      <Input
+        type="number"
+        id="itemQuantity"
+        value={itemQuantity}
+        onChange={(event) => setItemQuantity(event.target.value)}
+      />
+    </Box>
+    <Button type="submit" colorScheme="green" mb="5">
+      Create Invoice
+    </Button>
+    {errorMessage && <Text color="red">{errorMessage}</Text>}
+  </Box>
+  <Table variant="simple" mt="5">
+    <Thead>
+      <Tr>
+        <Th>Customer Name</Th>
+        <Th>Invoice Number</Th>
+        <Th>Item Name</Th>
+        <Th>Item Price</Th>
+        <Th>Item Quantity</Th>
+        <Th>Total</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      {invoices.map((invoice) => (
+        <Tr key={invoice.id}>
+          <Td>{invoice.customerName}</Td>
+          <Td>{invoice.invoiceNumber}</Td>
+          <Td>{invoice.items?.[0]?.name}</Td>
+          <Td>{invoice.items?.[0]?.price}</Td>
+          <Td>{invoice.items?.[0]?.quantity}</Td>
+          <Td>{invoice.total}</Td>
+        </Tr>
+      ))}
+    </Tbody>
+  </Table>
+  </Container>
+</>
 )
     }
     
